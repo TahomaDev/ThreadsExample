f130518d854fc5a87bd427ce96ba6c6217548fa0
Tahoma <tahomadev@gmail.com>
2022-11-18 17:45:56 +0300

    [ADD] GraphTask

---
+++ Source/TestThread/SRCGraphTaskExample.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SRCGraphTaskExample.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ASRCGraphTaskExample::ASRCGraphTaskExample()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASRCGraphTaskExample::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATestThreadGameModeBase *GameModeRef = Cast<ATestThreadGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+
+	if (GameModeRef != nullptr)
+	{
+		TaskDelegate.BindUFunction(this, FName("OnWorkDone"));
+/*		TGraphTask<FTask_GraphTask>::CreateTask(nullptr, ENamedThreads::AnyThread)
+			.ConstructAndDispatchWhenReady(TaskDelegate, GameModeRef, &Counter, 321);
+*/
+		MyCurrentTask = TGraphTask<FTask_GraphTask>::CreateTask(nullptr, ENamedThreads::AnyThread)
+			.ConstructAndHold(TaskDelegate, GameModeRef, &Counter, 321);
+
+	}
+	
+}
+
+// Called every frame
+void ASRCGraphTaskExample::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASRCGraphTaskExample::OnWorkDone(int32 Result)
+{
+	MyCurrentTask = nullptr;
+	OnWorkDoneBP(Result);
+}
+
+void ASRCGraphTaskExample::OnWorkDoneBP_Implementation(int32 Result)
+{
+	// IN BP
+}
+
+void ASRCGraphTaskExample::StartAsyncWork()
+{
+	if (MyCurrentTask != nullptr && !MyCurrentTask->GetCompletionEvent().IsValid())
+	{
+		MyCurrentTask->Unlock();
+	}
+	else
+	{
+		ATestThreadGameModeBase *GameModeRef = Cast<ATestThreadGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+
+		if (GameModeRef != nullptr)
+		{
+			TGraphTask<FTask_GraphTask>::CreateTask(nullptr, ENamedThreads::AnyThread)
+				.ConstructAndDispatchWhenReady(TaskDelegate, GameModeRef, &Counter, 321);
+		}
+	}
+}
+
---
+++ Source/TestThread/SRCGraphTaskExample.h
@@ -0,0 +1,155 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TestThreadGameModeBase.h"
+#include "GameFramework/Actor.h"
+#include "SRCGraphTaskExample.generated.h"
+
+DECLARE_DELEGATE_OneParam(FTaskDelegate_OnWorkDone, int32 OutputResult);
+
+#pragma optimize("", off) // only for breakpoints
+class FTask_FinishWork
+{
+	FTaskDelegate_OnWorkDone TaskDelegate_OnWorkDone;
+	ATestThreadGameModeBase* GameModeRef;
+	int32 Result;
+	
+public:
+	FTask_FinishWork(FTaskDelegate_OnWorkDone InTaskDelegate_OnWorkDone, ATestThreadGameModeBase* InGameModeRef, int32 InSimpleOutput) // CAUTION!: Must not use references in the constructor args; use pointers instead if you need by reference
+		: TaskDelegate_OnWorkDone(InTaskDelegate_OnWorkDone), GameModeRef(InGameModeRef), Result(InSimpleOutput)
+	
+	{
+		// Usually the constructor doesn't do anything except save the arguments for use in DoWork or GetDesiredThread.
+		UE_LOG(LogTemp, Warning, TEXT("FTask_FinishWork constructor"));
+	}
+	~FTask_FinishWork()
+	{
+		// you will be destroyed immediately after you execute. Might as well do cleanup in DoWork, but you could also use a destructor.
+		UE_LOG(LogTemp, Warning, TEXT("~FTask_FinishWork destructor"));
+	}
+	FORCEINLINE TStatId GetStatId() const
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(FGenericTask, STATGROUP_TaskGraphTasks);
+	}
+
+	static ENamedThreads::Type GetDesiredThread()
+	{
+
+		return ENamedThreads::GameThread;
+	}
+
+	void ThreadWork()
+	{
+
+	}
+
+	static ESubsequentsMode::Type GetSubsequentsMode() { return ESubsequentsMode::FireAndForget; };
+	
+	void DoTask(ENamedThreads::Type CurrentThread, const FGraphEventRef& MyCompletionGraphEvent)
+	{
+		check(IsInGameThread())
+		if (TaskDelegate_OnWorkDone.IsBound())
+		{
+			TaskDelegate_OnWorkDone.Execute(Result);
+		}
+		
+	}
+};
+
+class FTask_GraphTask
+{
+	FTaskDelegate_OnWorkDone TaskDelegate_OnWorkDone;
+	ATestThreadGameModeBase* GameModeRef;
+	int32 *SimpleOutput;
+	int32 Number;
+	
+public:
+	FTask_GraphTask(FTaskDelegate_OnWorkDone InTaskDelegate_OnWorkDone, ATestThreadGameModeBase* InGameModeRef, int32 *InSimpleOutput, int32 InNumber) // CAUTION!: Must not use references in the constructor args; use pointers instead if you need by reference
+		: TaskDelegate_OnWorkDone(InTaskDelegate_OnWorkDone), GameModeRef(InGameModeRef), SimpleOutput(InSimpleOutput), Number(InNumber)
+	
+	{
+		// Usually the constructor doesn't do anything except save the arguments for use in DoWork or GetDesiredThread.
+		UE_LOG(LogTemp, Warning, TEXT("FTask_GraphTask constructor"));
+	}
+	~FTask_GraphTask()
+	{
+		// you will be destroyed immediately after you execute. Might as well do cleanup in DoWork, but you could also use a destructor.
+		UE_LOG(LogTemp, Warning, TEXT("~FTask_GraphTask destructor"));
+	}
+	FORCEINLINE TStatId GetStatId() const
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(FGenericTask, STATGROUP_TaskGraphTasks);
+	}
+
+	static ENamedThreads::Type GetDesiredThread()
+	{
+		 FAutoConsoleTaskPriority CPrio_MyGraphAsyncTask(
+		TEXT("TaskGraph.Test.ExampleTask"),
+		TEXT("TaskGraph.Test.ExampleTask help text"),
+			ENamedThreads::BackgroundThreadPriority, // if we have background priority task threads, then use them...
+			ENamedThreads::NormalTaskPriority, // .. at normal task priority
+			ENamedThreads::NormalTaskPriority // if we don't have background threads, then use normal priority threads at normal task priority instead
+			);
+		
+		return CPrio_MyGraphAsyncTask.Get(); // ENamedThreads::AnyBackgroundThreadNormalTask;
+	}
+
+	void ThreadWork()
+	{
+		int32 i = 0;
+		while (i < 500)
+		{
+			FPlatformProcess::Sleep(0.001f);
+			*SimpleOutput = (*SimpleOutput + 1);
+			i++;
+		}
+	}
+
+	static ESubsequentsMode::Type GetSubsequentsMode() { return ESubsequentsMode::FireAndForget; };
+	
+	void DoTask(ENamedThreads::Type CurrentThread, const FGraphEventRef& MyCompletionGraphEvent)
+	{
+		ThreadWork();
+		TGraphTask<FTask_FinishWork>::CreateTask(nullptr, CurrentThread).ConstructAndDispatchWhenReady(TaskDelegate_OnWorkDone, GameModeRef, *SimpleOutput);
+		
+		// The arguments are useful for setting up other tasks. 
+		// Do work here, probably using SomeArgument.
+		//MyCompletionGraphEvent->DontCompleteUntil(TGraphTask<FSomeChildTask>::CreateTask(NULL,CurrentThread).ConstructAndDispatchWhenReady());
+	}
+};
+#pragma optimize("", on)
+
+UCLASS()
+class TESTTHREAD_API ASRCGraphTaskExample : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASRCGraphTaskExample();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnWorkDoneBP(int32 Result);
+	UFUNCTION()
+	void OnWorkDone(int32 Result);
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 Counter = 0;
+	FTaskDelegate_OnWorkDone TaskDelegate;
+
+	TGraphTask<FTask_GraphTask> *MyCurrentTask;
+
+	UFUNCTION(BlueprintCallable)
+	void StartAsyncWork();
+};
+
